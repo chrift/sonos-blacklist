883f3e4773c2658c6f39d83e76cbf0a463a1eb74
Chris Cheshire <chris@ratio.co.uk>
2018-01-24 12:48:27 +0000

    Event driven - no delays between skips

--- index.js
+++ index.js
@@ -1,187 +1,180 @@
-var fs   = require('fs'),
-    path = require('path');
+const fs = require('fs'),
+  path = require('path');
 
-var Sonos = require('sonos').Sonos;
+const sonos = require('sonos');
 
-var _      = require('underscore'),
-    async  = require('async'),
-    moment = require('moment');
+const Debug = require('debug'),
+  _ = require('lodash'),
+  moment = require('moment');
 
-var ips = [
-	{
-		ip: '10.41.10.102',
-		sonos: null,
-		currentTrack: null
-	},
-	{
-		ip: '10.41.10.125',
-		sonos: null,
-		currentTrack: null
-	},
-	{
-		ip: '10.41.10.144',
-		sonos: null,
-		currentTrack: null
-	}
-];
+const debug = Debug('blacklist:index');
 
-var counterFile,
-    counter;
+let counterFile,
+  counter,
+  counterChanged = false;
+
+const blacklistFilePath = path.join(__dirname, 'blacklist.json'),
+  devices = [];
 
 _setCounter();
 
-var counterChanged = false;
+debug('Discovering sonos devices...');
 
-_.each(ips, function (obj) {
-	obj.sonos = new Sonos(obj.ip, 1400);
+debug(blacklistFilePath);
+
+fs.watchFile(blacklistFilePath, () => {
+  debug('Blacklist change detected');
+
+  devices.forEach(device =>
+    device.currentTrack((err, track) => checkSongInBlacklist(device, track))
+  );
 });
 
-async.forever(function (nextForever) {
-	var blacklist = _getBlacklist();
+sonos.search((device) => {
+  devices.push(device);
 
-	async.each(ips, function (obj, nextIP) {
-		obj.sonos.currentTrack(function (err, response) {
-			if(err)
-				console.error(err);
+  debug(`Sonos device found with ip ${device.host}`);
 
-			if (!response || !response.title)
-				return nextIP();
-
-			console.log(response.title);
-
-			var foundInBlacklist = false;
-
-			_.each(blacklist, function (track) {
-				if (foundInBlacklist) //Don't go any further if we've already found this song in our blacklist
-					return;
-
-				if (typeof track === 'object') {
-					var matchSongBlacklist   = false,
-					    matchArtistBlacklist = false;
-
-					if (!track.title) {
-						matchSongBlacklist = true;
-					} else if (track.title && response.title) {
-						var titleRGX = new RegExp(escapeRegExp(track.title), 'gi');
-
-						matchSongBlacklist = !!response.title.match(titleRGX)
-					}
-
-					if (!track.artist) {
-						matchArtistBlacklist = true;
-					} else if (track.artist && response.artist) {
-						var artistRGX = new RegExp(escapeRegExp(track.artist), 'gi');
-
-						matchArtistBlacklist = !!response.artist.match(artistRGX);
-					}
-
-					//If it passed all of our tests, blacklist the shit out of it!
-					if (matchSongBlacklist && matchArtistBlacklist) {
-						console.log('found in blacklist', response.title);
-						foundInBlacklist = true;
-					}
-				} else {
-					var rgx = new RegExp(escapeRegExp(track), 'gi');
-
-					if (response.title.match(rgx))
-						foundInBlacklist = true;
-				}
-
-				if (!foundInBlacklist)
-					return;
-
-				console.log('About to skip');
-
-				foundInBlacklist = true;
-
-				incrementSong(response, true);
-
-				obj.sonos.next(function (err, nextResponse) {
-					console.log('next: ', err, nextResponse);
-
-					if (err) {
-						obj.sonos.pause(function (err, response) {
-							console.log('pause: ', err, response);
-
-							return setTimeout(nextIP, 8000);
-						})
-					} else {
-						return setTimeout(nextIP, 8000);
-					}
-				});
-			});
-
-			if (!foundInBlacklist) {
-				if (obj.currentTrack && (obj.currentTrack.title !== response.title || obj.currentTrack.artist !== response.artist))
-					incrementSong(response);
-
-				obj.currentTrack = response;
-
-				return nextIP();
-			}
-		});
-	}, function () {
-		_saveCounter();
-
-		setTimeout(nextForever, 2000);
-	});
-}, function () {
-	console.log('error');
+  device.on('TrackChanged', track => checkSongInBlacklist(device, track));
 });
+
+function checkSongInBlacklist(device, song) {
+  if (!song || !song.title) {
+    return;
+  }
+
+  const sdebug = Debug(`blacklist:${device.host}`);
+
+  sdebug(song.title);
+
+  const blacklist = _getBlacklist();
+
+  let foundInBlacklist = false;
+
+  _.each(blacklist, function (track) {
+    if (foundInBlacklist) //Don't go any further if we've already found this song in our blacklist
+    {
+      return;
+    }
+
+    if (typeof track === 'object') {
+      let matchSongBlacklist = false,
+        matchArtistBlacklist = false;
+
+      if (!track.title) {
+        matchSongBlacklist = true;
+      } else if (track.title && song.title) {
+        const titleRGX = new RegExp(escapeRegExp(track.title), 'gi');
+
+        matchSongBlacklist = !!song.title.match(titleRGX);
+      }
+
+      if (!track.artist) {
+        matchArtistBlacklist = true;
+      } else if (track.artist && song.artist) {
+        const artistRGX = new RegExp(escapeRegExp(track.artist), 'gi');
+
+        matchArtistBlacklist = !!song.artist.match(artistRGX);
+      }
+
+      //If it passed all of our tests, blacklist the shit out of it!
+      if (matchSongBlacklist && matchArtistBlacklist) {
+        sdebug('found in blacklist', song.title);
+        foundInBlacklist = true;
+      }
+    } else {
+      const rgx = new RegExp(escapeRegExp(track), 'gi');
+
+      if (song.title.match(rgx)) {
+        foundInBlacklist = true;
+      }
+    }
+
+    if (!foundInBlacklist) {
+      return;
+    }
+
+    sdebug('About to skip');
+
+    foundInBlacklist = true;
+
+    incrementSong(song, true);
+
+    device.next(function (err, nextResponse) {
+      sdebug('next: ', err, nextResponse);
+
+      if (err) {
+        device.pause(function (err, response) {
+          sdebug('pause: ', err, response);
+        });
+      }
+    });
+  });
+
+  if (!foundInBlacklist) {
+    incrementSong(song);
+
+    _saveCounter();
+  }
+}
 
 function incrementSong(songObj, skipped) {
-	counterChanged = true;
+  counterChanged = true;
 
-	var songCounter = counter[songObj.title + ' - ' + songObj.artist] = counter[songObj.title + ' - ' + songObj.artist] || songObj;
+  const songCounter = counter[songObj.title + ' - ' + songObj.artist] = counter[songObj.title + ' - ' + songObj.artist] || songObj;
 
-	songCounter.skipped = songCounter.skipped || 0;
-	songCounter.count = songCounter.count || 0;
+  songCounter.skipped = songCounter.skipped || 0;
+  songCounter.count = songCounter.count || 0;
 
-	if (skipped) {
-		songCounter.skipped++;
-	} else {
-		songCounter.count++;
-	}
+  if (skipped) {
+    songCounter.skipped++;
+  } else {
+    songCounter.count++;
+  }
 }
 
 function _getTodaysCounterPath() {
-	return path.join(__dirname, 'counters', moment().startOf('day').format() + '.json');
+  return path.join(__dirname, 'counters', moment()
+    .startOf('day')
+    .format() + '.json');
 }
 
 function _setCounter() {
-	counterFile = _getTodaysCounterPath();
+  counterFile = _getTodaysCounterPath();
 
-	try {
-		fs.statSync(counterFile)
-	} catch (e) {
-		fs.writeFileSync(counterFile, '{}');
-	}
+  try {
+    fs.statSync(counterFile);
+  } catch (e) {
+    fs.writeFileSync(counterFile, '{}');
+  }
 
-	try {
-		counter = JSON.parse(fs.readFileSync(counterFile).toString());
-	} catch (e) {
-		counter = {};
-	}
+  try {
+    counter = JSON.parse(fs.readFileSync(counterFile)
+      .toString());
+  } catch (e) {
+    counter = {};
+  }
 }
 
 function _saveCounter() {
-	if (counterChanged) {
-		counterChanged = false;
+  if (counterChanged) {
+    counterChanged = false;
 
-		fs.writeFileSync(counterFile, JSON.stringify(counter));
+    fs.writeFileSync(counterFile, JSON.stringify(counter));
 
-		_setCounter();
-	}
+    _setCounter();
+  }
 }
 
 function _getBlacklist() {
-	return JSON.parse(fs.readFileSync(path.join(__dirname, 'blacklist.json')).toString()) || [];
+  return JSON.parse(fs.readFileSync(blacklistFilePath)
+    .toString()) || [];
 }
 
 function escapeRegExp(str) {
-	return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
 }
 
-console.log('Watching...');
+debug('Watching...');
 
 process.stdin.resume();
